48d5159a48709a9e0da185fae3718c59a60eb28f
HiranMadhu <hiranabeywardhana95@gmail.com>
2024-12-16 11:20:54 +0530

    change the mapping

--- src/main/java/com/integrationninjas/springbootexample/controller/PatientController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/PatientController.java
@@ -12,18 +12,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/patient")
 public class PatientController {
  
 	@Autowired
 	private PatientService patientService;
 
-	@GetMapping("/patient/")
+	@GetMapping("/users")
 	public ResponseEntity<List<PatientDto>> getPatients() {
 		List<PatientDto> patientsList = patientService.getPatients();
 		return new ResponseEntity<>(patientsList, HttpStatus.OK);
 	}
 
-	@PostMapping("/patient/")
+	@PostMapping("/users")
 	public ResponseEntity<String> createPatient(@RequestBody PatientDto patientDto) {
 		try {
 			String status = patientService.createPatient(patientDto);
